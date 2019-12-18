9e542909c84cd43d8f07fe6bdc9d2efc099bd95e
TranXuanTu1003 <tutx.nde18051@vtc.edu.vn>
2019-12-18 18:43:27 +0700

    aa

---
+++ PL_Console/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter UserName: ");
+            string UserName = Console.ReadLine();
+            Console.Write("Enter PassWord: ");
+            string source = InputPassword();
+            using (MD5 md5Hash = MD5.Create())
+            {
+                string hash = GetMd5Hash(md5Hash, source);
+
+                Console.WriteLine("\nThe MD5 hash of password is: " + hash);
+
+                Console.WriteLine("Verifying the hash...");
+                Console.WriteLine("Login successful, Press enter to continue...");
+
+                if (VerifyMd5Hash(md5Hash, source, hash))
+                {
+                    Console.WriteLine("The hashes are the same.");
+                }
+                else
+                {
+                    Console.WriteLine("The hashes are not same.");
+                }
+            }
+        }
+        static string InputPassword()
+        {
+            //ABC
+            string InputPass = "";
+            do
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                {
+                    InputPass += key.KeyChar;
+                    Console.Write("*");
+                }
+                else
+                {
+                    if (key.Key == ConsoleKey.Backspace && InputPass.Length > 0)
+                    {
+                        InputPass = InputPass.Substring(0, (InputPass.Length - 1));
+                        Console.Write("\b \b");
+                    }
+                    else if (key.Key == ConsoleKey.Enter)
+                    {
+                        break;
+                    }
+                }
+            }
+            while (true);
+            return InputPass;
+        }
+        static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
+        // Verify a hash against a string.
+        static bool VerifyMd5Hash(MD5 md5Hash, string input, string hash)
+        {
+            // Hash the input.
+            string hashOfInput = GetMd5Hash(md5Hash, input);
+
+            // Create a StringComparer an compare the hashes.
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            if (0 == comparer.Compare(hashOfInput, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
